5245901dd5889802e7148d3dd88e3bff144aeb95
kkhushie <khushipal1470@gmail.com>
2026-01-15 00:39:01 +0530

    Setup Google OAuth Implement basic login Log authenticated user email Apply domain restriction logic

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+// const allowedDomains = ["dau.ac.in", "daiict.ac.in"];
+
+const handler = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: "openid email profile",
+        },
+      },
+    
+    }),
+  ],
+
+  callbacks: {
+    async signIn({ user }) {
+      if (!user?.email) {
+        console.log("No email returned from provider");
+        return false;
+      }
+    
+      const allowedDomains = ["dau.ac.in", "daiict.ac.in"];
+      const domain = user.email.split("@")[1];
+    
+      return allowedDomains.includes(domain);
+    },
+    
+
+    async session({ session }) {
+      console.log("LOGGED IN EMAIL:", session?.user?.email);
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
